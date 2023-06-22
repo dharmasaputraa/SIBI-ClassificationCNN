cea6c0921e9402a28faa65fd22e28c36d1a2cda0
dharmasaputraa <putudharma0@gmail.com>
2023-06-23 03:55:44 +0800

    first commit

---
+++ app.py
@@ -0,0 +1,140 @@
+import PySimpleGUI as sg
+import os.path
+from PIL import Image, ImageOps
+import tempfile
+
+from processing import *
+
+sg.theme("DefaultNoMoreNagging")
+
+op_button = ""
+coldepth = 0
+img_input = None
+
+# Kolom Area No 1: Area open folder and select image
+file_list_column = [
+    [
+        sg.Frame(
+            "Image Information",
+            [
+                [sg.Text("Image Size : "), sg.Text(size=(13, 1), key="ImgSize")],
+                [sg.Text("Color Depth : "), sg.Text(size=(13, 1), key="ImgColorDepth")],
+            ],
+            font=("OpenSans", 10),
+        )
+    ],
+    [
+        sg.Frame(
+            "Choose image",
+            [
+                [
+                    sg.In(size=(19, 1), enable_events=True, key="ImgFolder"),
+                    sg.FolderBrowse(),
+                ],
+                [
+                    sg.Listbox(
+                        values=[], enable_events=True, size=(25, 10), key="ImgList"
+                    )
+                ],
+            ],
+            font=("OpenSans", 10),
+        )
+    ],
+]
+# Kolom Area No 2: Area viewer image input
+image_viewer_column = [
+    [sg.Text("Image Input :")],
+    [sg.Text(size=(40, 1), key="FilepathImgInput")],
+    [sg.Image(key="ImgInputViewer")],
+]
+# Kolom Area No 3: Area Image info dan Tombol list of processing
+list_processing = [
+    [sg.Text(size=(11, 11), key="Prediction")],
+    [
+        sg.Button("Classification", size=(11, 1), key="Classification"),
+    ],
+]
+
+# Kolom Area No 4: Area viewer image output
+image_viewer_column2 = [
+    [sg.Text("Image Input:")],
+    [sg.Text(size=(40, 1), key="FilepathImgInput")],
+    [sg.Image(key="ImgInputViewer")],
+]
+
+# Gabung Full layout
+layout = [
+    [
+        sg.Column(file_list_column),
+        sg.VSeperator(),
+        sg.Column(image_viewer_column2),
+        sg.VSeperator(),
+        sg.Column(list_processing),
+    ]
+]
+
+window = sg.Window("Classification Model CNN by Dharma Saputra", layout)
+# Run the Event Loop
+
+# nama image file temporary setiap kali processing output
+filename_out = "out.png"
+
+while True:
+    event, values = window.read()
+
+    if event == "Exit" or event == sg.WIN_CLOSED:
+        break
+
+    # Folder name was filled in, make a list of files in the folder
+    if event == "ImgFolder":
+        folder = values["ImgFolder"]
+
+        try:
+            # Get list of files in folder
+            file_list = os.listdir(folder)
+        except:
+            file_list = []
+
+        fnames = [
+            f
+            for f in file_list
+            if os.path.isfile(os.path.join(folder, f))
+            and f.lower().endswith((".png", ".gif", ".jpg", ".jpeg"))
+        ]
+
+        window["ImgList"].update(fnames)
+
+    elif event == "ImgList":  # A file was chosen from the listbox
+        try:
+            filename = os.path.join(values["ImgFolder"], values["ImgList"][0])
+            # print(values["ImgList"][0])
+            window["FilepathImgInput"].update(filename)
+
+            # Convert image to PNG format and resize to 64 x 64
+            with Image.open(filename) as img:
+                img_resized = img.resize((256, 256))
+                temp_file = tempfile.NamedTemporaryFile(suffix=".png", delete=False)
+                temp_filename = temp_file.name
+                img_resized.save(temp_filename)
+
+            # Update image viewer
+            window["ImgInputViewer"].update(filename=temp_filename)
+
+            # Update image information
+            img_input = Image.open(temp_filename)
+            img_width, img_height = img_input.size
+            window["ImgSize"].update(str(img_width) + " x " + str(img_height))
+            coldepth = img_input.mode
+            window["ImgColorDepth"].update(str(coldepth))
+        except:
+            pass
+    elif event == "Classification":
+        # try:
+        prediction = Classification(values["ImgList"][0])
+        print(prediction)
+        window["Prediction"].update(prediction)
+    # except:
+    #     pass
+
+
+# github.com/dharmasaputraa
---
+++ processing.py
@@ -0,0 +1,107 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import cv2
+import os
+import tensorflow as tf
+from keras.layers import Conv2D, Dense, Dropout, Flatten, MaxPooling2D
+from keras.models import Sequential
+from keras.preprocessing.image import ImageDataGenerator
+from sklearn.preprocessing import MultiLabelBinarizer
+
+labels = [
+    "A",
+    "B",
+    "C",
+    "D",
+    "E",
+    "F",
+    "G",
+    "H",
+    "I",
+    "J",
+    "K",
+    "L",
+    "M",
+    "N",
+    "O",
+    "P",
+    "Q",
+    "R",
+    "S",
+    "T",
+    "U",
+    "V",
+    "W",
+    "X",
+    "Y",
+    "Z",
+]
+
+mlb = MultiLabelBinarizer()
+mlb.fit(labels)
+mlb.classes_
+
+
+def CNN():
+    # inisiasi CNN
+    model = Sequential()
+
+    # Convolution dan Max Pooling
+    model.add(Conv2D(35, (3, 3), input_shape=(64, 64, 1), activation="relu"))
+    model.add(MaxPooling2D(pool_size=(2, 2)))  # 2x2 is optimal
+
+    model.add(Dropout(0.5))
+    model.add(Conv2D(64, (3, 3), activation="relu"))
+    model.add(MaxPooling2D(pool_size=(2, 2)))
+
+    model.add(Conv2D(128, (3, 3), activation="relu"))
+    model.add(MaxPooling2D(pool_size=(2, 2)))
+
+    model.add(Dropout(0.5))
+    model.add(Conv2D(256, (3, 3), activation="relu"))
+    model.add(MaxPooling2D(pool_size=(2, 2)))
+
+    # Flattening
+    model.add(Flatten())
+
+    # Dense atau Full Connection
+    model.add(Dropout(0.5))
+    model.add(Dense(512, activation="relu"))
+    model.add(Dropout(0.5))
+    model.add(Dense(units=26, activation="softmax"))
+
+    return model
+
+
+SibiKlasifikasi = CNN()
+SibiKlasifikasi.load_weights("model\model_sign_e75.h5")
+
+dataset_path = (
+    "C:/Users/putud/Desktop/Kuliah/Semester 4/Kecerdasan Buatan/UAS/2/app/database"
+)
+data_test = os.listdir(dataset_path)
+
+
+def getLabel(label, model_label):
+    for i, x in enumerate(model_label):
+        if x >= 0.6:
+            return label[i]
+
+    return "Gambar bukan abjad SIBI"
+
+
+def Classification(img_input):
+    hasil = data_test
+    for x in range(len(hasil)):
+        if hasil[x] == img_input:
+            img = cv2.imread(dataset_path + "/" + hasil[x])
+            img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+            resize = cv2.resize(img, dsize=(64, 64))
+            img = np.expand_dims(resize, axis=0)
+            predict = SibiKlasifikasi.predict(img)
+            label = (hasil[x], getLabel(mlb.classes_, predict[0]))
+            hasil.append(label)
+            return hasil[x][0]
+
+    return "ga ada yang cocok bang"
